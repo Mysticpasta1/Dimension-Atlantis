e3723c1087ffd6522ea1a4bb76165cd67c8e372d
Mysticpasta1 <ethana5tc@outlook.com>
2024-07-13 09:27:38 -0500

    shoulld work now

--- src/main/java/com/mystic/atlantis/mixin/RedstoneWireBlockMixin.java
+++ src/main/java/com/mystic/atlantis/mixin/RedstoneWireBlockMixin.java
@@ -3,6 +3,7 @@ package com.mystic.atlantis.mixin;
 import com.mystic.atlantis.init.BlockInit;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
@@ -24,7 +25,7 @@ public abstract class RedstoneWireBlockMixin {
         return state.is(block) || state.is(BlockInit.ATLANTEAN_POWER_DUST_WIRE.get());
     }
 
-    @Redirect(method = "getWireSignal", at = @At(value = "HEAD"))
+    @Unique
     private int getWireSignal(BlockState state) {
         if (state.is(Blocks.REDSTONE_WIRE)) {
             return state.getValue(RedStoneWireBlock.POWER);
