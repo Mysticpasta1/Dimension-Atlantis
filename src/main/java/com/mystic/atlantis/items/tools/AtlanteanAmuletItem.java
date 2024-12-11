d03c4cb20f56b324b18c05b1b0912d1492430ec5
Mysticpasta1 <ethana5tc@outlook.com>
2024-12-11 15:08:52 -0600

    fix amulet again

--- src/main/java/com/mystic/atlantis/items/tools/AtlanteanAmuletItem.java
+++ src/main/java/com/mystic/atlantis/items/tools/AtlanteanAmuletItem.java
@@ -8,22 +8,20 @@ import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 
-import java.util.function.Consumer;
-
 public class AtlanteanAmuletItem extends DefaultItem {
     public AtlanteanAmuletItem() {
-        super(new Properties().stacksTo(1).durability(250));
+        super(new Properties().stacksTo(1).durability(250).fireResistant());
     }
 
     @Override
     public void inventoryTick(ItemStack stack, Level pLevel, Entity pEntity, int pSlotId, boolean pIsSelected) {
         if (pEntity instanceof LivingEntity livingEntity) {
-            MobEffectInstance mobEffect = new MobEffectInstance(MobEffects.FIRE_RESISTANCE, 120, 1, false, false);
+            MobEffectInstance mobEffect = new MobEffectInstance(MobEffects.FIRE_RESISTANCE, 20, 1, false, false);
             livingEntity.addEffect(mobEffect);
-            if(livingEntity.isOnFire()) {
-                stack.getItem().damageItem(stack, 1, livingEntity, livingEntity1 -> {
-                    livingEntity.removeEffect(mobEffect.getEffect());
-                });
+            if(stack.getItem() instanceof AtlanteanAmuletItem) {
+                if (pEntity.tickCount % 20 == 0) {
+                    stack.hurtAndBreak(1, livingEntity, livingEntity1 -> {});
+                }
             }
         }
     }
